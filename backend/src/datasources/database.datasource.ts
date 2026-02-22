7f63c1bbe92a01aa7cfaf6b67c78c893c714eee5
Andrey <andrepud@mail.ru>
2026-02-22 21:51:05 +0300

    поправил process.env.DATABASE_URL

--- backend/src/datasources/database.datasource.ts
+++ backend/src/datasources/database.datasource.ts
@@ -4,18 +4,14 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'database',
   connector: 'postgresql',
-  url: '',
-  host: 'db',
-  port: 5432,
-  user: 'postgres',
-  password: 'psql',
-  database: 'pizza',
+  url: process.env.DATABASE_URL || '',
+  host: process.env.DB_HOST || process.env.PGHOST || 'db',
+  port: parseInt(process.env.DB_PORT || process.env.PGPORT || '5432'),
+  user: process.env.DB_USER || process.env.PGUSER || 'postgres',
+  password: process.env.DB_PASSWORD || process.env.PGPASSWORD || 'psql',
+  database: process.env.DB_DATABASE || process.env.PGDATABASE || 'pizza',
 };
 
-// Observe application's life cycle to disconnect the datasource when
-// application is stopped. This allows the application to be shut down
-// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class DatabaseDataSource extends juggler.DataSource implements LifeCycleObserver {
   static dataSourceName = 'database';
